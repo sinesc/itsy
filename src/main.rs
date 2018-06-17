a0336c6cec80b5b06a0abfb5befea3eecc9d322f
Dennis Möhlmann <dmo479@gmail.com>
2018-06-17 13:59:31 +0200

    parse binary expressions, use CompleteStr

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ extern crate nom;
 mod parser;
 use parser::*;
 
+use nom::types::CompleteStr as Input;
+
 fn main() {
-    println!("{:?}", function("fn hello(name: u8, mut greeting: u16) -> u8 { placeholder } ."));
-    println!("{:?}", function("fn hello(name: u8, mut greeting: u16) { placeholder } ."));
-    println!("{:?}", structure("struct mystruct { } ."));
-    println!("{:?}", structure("struct mystruct { name: string } ."));
-    println!("{:?}", structure("struct mystruct { name: string, count: u8 } ."));
+    println!("{:?}", function(Input("fn hello(name: u8, mut greeting: u16) -> u8 { placeholder }")));
+    println!("{:?}", function(Input("fn hello(name: u8, mut greeting: u16) { placeholder }")));
+    println!("{:?}", structure(Input("struct mystruct { }")));
+    println!("{:?}", structure(Input("struct mystruct { name: string }")));
+    println!("{:?}", structure(Input("struct mystruct { name: string, count: u8 }")));
+    println!("{:?}", expression(Input("3+5*2-8")));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,12 @@
 use nom::*;
+use nom::types::CompleteStr as Input;
 use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum AST<'a> {
+    Block(Block<'a>),
+    Function(Function<'a>),
+}
 
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
@@ -69,57 +76,97 @@ pub struct Function<'a> {
 }
 
 #[derive(Debug)]
-pub enum AST<'a> {
-    Block(Block<'a>),
-    Function(Function<'a>),
+pub enum Expression {
+    Ident(String),                                      // TODO: would like these to be &str, caused lifetime issue in fold_many0 closure though
+    Literal(String),
+    Operation(Operation),
+}
+
+#[derive(Debug)]
+pub enum Operation {
+    Binary(String, Box<Expression>, Box<Expression>),   // TODO: would like these to be &str, caused lifetime issue in fold_many0 closure though
+    Prefix(String, Box<Expression>),
+    Suffix(String, Box<Expression>),
 }
 
 // basics
 
-named!(ident<&str, &str>, take_while!(|tw| is_alphanumeric(tw as u8))); // TODO: more like [_a-zA-Z][_a-zA-Z0-9]*
-named!(ty<&str, Type>, map!(ident, |ident| Type::from_string(ident)));
-named!(mutable<&str, &str>, tag!("mut"));
-named!(type_assign_op<&str, &str>, tag!(":"));
-named!(list_separator<&str, &str>, tag!(","));
+named!(ident<Input, Input>, take_while!(|tw| is_alphanumeric(tw as u8))); // TODO: more like [_a-zA-Z][_a-zA-Z0-9]*
+named!(ty<Input, Type>, map!(ident, |ident| Type::from_string(*ident)));
+named!(mutable<Input, Input>, tag!("mut"));
+named!(type_assign_op<Input, Input>, tag!(":"));
+named!(list_separator<Input, Input>, tag!(","));
 
 // block
 
-named!(block_items<&str, Block>, map!(ws!(opt!(tag!("placeholder"))), |items| Block { // TODO: implement
+named!(block_items<Input, Block>, map!(ws!(opt!(tag!("placeholder"))), |items| Block { // TODO: implement
     statements: Vec::new()
 }));
 
-named!(block<&str, Block>, ws!(delimited!(tag!("{"), block_items, tag!("}"))));
+named!(block<Input, Block>, ws!(delimited!(tag!("{"), block_items, tag!("}"))));
+
+// expression
+
+named!(parens<Input, Expression>, ws!(delimited!(tag!("("), expression, tag!(")"))));
+
+named!(ident_exp<Input, Expression>, map!(ident, |m| Expression::Ident(m.to_string())));
+
+named!(operand<Input, Expression>, alt!(ident_exp | parens));
+
+named!(term<Input, Expression>, do_parse!(
+    init: operand >>
+    res: fold_many0!(
+        pair!(alt!(tag!("*") | tag!("/")), operand),
+        init,
+        |acc, (op, val): (Input, Expression)| {
+            Expression::Operation(Operation::Binary(op.to_string(), Box::new(acc), Box::new(val)))
+        }
+    ) >>
+    (res)
+));
+
+named!(pub expression<Input, Expression>, do_parse!(
+    init: term >>
+    res: fold_many0!(
+        pair!(alt!(tag!("+") | tag!("-")), term),
+        init,
+        |acc, (op, val): (Input, Expression)| {
+            Expression::Operation(Operation::Binary(op.to_string(), Box::new(acc), Box::new(val)))
+        }
+    ) >>
+    (res)
+));
 
 // structure
 
-named!(structure_item<&str, (&str, Type)>, map!(ws!(tuple!(ident, type_assign_op, ty)), |tuple| (tuple.0, tuple.2)));
+named!(structure_item<Input, (Input, Type)>, map!(ws!(tuple!(ident, type_assign_op, ty)), |tuple| (tuple.0, tuple.2)));
 
-named!(structure_items<&str, HashMap<&str, Type>>, map!(ws!(separated_list_complete!(list_separator, structure_item)), |list| list.into_iter().collect()));
+named!(structure_items<Input, HashMap<&str, Type>>, map!(ws!(separated_list_complete!(list_separator, structure_item)), |list| list.into_iter().map(|item| (*item.0, item.1)).collect()));
 
-named!(pub structure<&str, Structure>, map!(ws!(tuple!(tag!("struct"), ident, tag!("{"), structure_items, tag!("}"))), |tuple| Structure {
-    name: tuple.1,
+named!(pub structure<Input, Structure>, map!(ws!(tuple!(tag!("struct"), ident, tag!("{"), structure_items, tag!("}"))), |tuple| Structure {
+    name: *tuple.1,
     items: tuple.3,
 }));
 
 // function
 
-named!(argument<&str, Argument>, map!(ws!(tuple!(opt!(mutable), ident, type_assign_op, ty)), |tuple| Argument {
-    name    : tuple.1,
+named!(argument<Input, Argument>, map!(ws!(tuple!(opt!(mutable), ident, type_assign_op, ty)), |tuple| Argument {
+    name    : *tuple.1,
     mutable : tuple.0.is_some(),
     ty      : tuple.3,
 }));
 
-named!(argument_list<&str, Vec<Argument>>, ws!(delimited!(tag!("("), separated_list_complete!(list_separator, argument), tag!(")"))));
+named!(argument_list<Input, Vec<Argument>>, ws!(delimited!(tag!("("), separated_list_complete!(list_separator, argument), tag!(")"))));
 
-named!(return_part<&str, Type>, ws!(preceded!(tag!("->"), ty)));
+named!(return_part<Input, Type>, ws!(preceded!(tag!("->"), ty)));
 
-named!(signature<&str, Signature>, map!(ws!(tuple!(tag!("fn"), ident, argument_list, opt!(return_part))), |sig| Signature {
-    name    : sig.1,
+named!(signature<Input, Signature>, map!(ws!(tuple!(tag!("fn"), ident, argument_list, opt!(return_part))), |sig| Signature {
+    name    : *sig.1,
     args    : sig.2,
     ret     : sig.3.unwrap_or(Type::void),
 }));
 
-named!(pub function<&str, Function>, map!(ws!(tuple!(signature, block)), |func| Function {
+named!(pub function<Input, Function>, map!(ws!(tuple!(signature, block)), |func| Function {
     sig: func.0,
     block: func.1,
 }));
