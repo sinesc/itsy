49dd88e53aab5a2c811725ca0904be93d1e86aa8
Dennis Möhlmann <dmo479@gmail.com>
2022-06-11 16:28:03 +0200

    add primitive enum cast test

--- tests/vm.rs
+++ tests/vm.rs
@@ -1661,6 +1661,26 @@ fn primitive_enum() {
 }
 
 #[test]
+fn primitive_enum_cast() {
+    let result = run("
+        enum Simple {
+            A = 3, B, C
+        }
+        fn main() {
+            let a = Simple::A;
+            ret_u8(a as u8);
+            ret_u8(Simple::B as u8);
+            if a as u8 == 3 {
+                ret_u8(1)
+            } else {
+                ret_u8(0);
+            }
+        }
+    ");
+    assert_all(&result, &[ 3u8, 4, 1 ]);
+}
+
+#[test]
 fn tempory_access() {
     let result = run("
         struct Test {
